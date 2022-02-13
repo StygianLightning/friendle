592e95d0ab34c18eee8f2854bd9cd4aea071851a
StygianLightning <denrellum@gmail.com>
2022-02-13 01:12:29 +0100

    Added game logic.

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,3 @@
 pub const WORD_LENGTH: usize = 5;
 
-pub const MAX_GUESSS: usize = 6;
+pub const MAX_GUESSES: usize = 6;
--- src/model/game.rs
+++ src/model/game.rs
@@ -1,12 +1,13 @@
 use super::evaluation::{evaluate, Evaluation};
 use super::validate_word::validate_word;
+use crate::constants::MAX_GUESSES;
 use anyhow::{bail, Result};
 use std::collections::HashSet;
 
 #[derive(Debug)]
 pub struct Guess {
     pub word: String,
-    pub evaluation: Evaluation,
+    pub evaluation: Vec<Evaluation>,
 }
 
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
@@ -41,8 +42,36 @@ impl Game {
         self.state
     }
 
-    pub fn guess(&mut self, guess: &str, word_list: &HashSet<String>) -> Result<()> {
-        evaluate(guess, &self.solution, word_list)?;
+    pub fn guess(&mut self, guess: String, word_list: &HashSet<String>) -> Result<()> {
+        if self.state != GameState::InProgress {
+            bail!("Game is already finished.");
+        }
+        let evaluation = evaluate(&guess, &self.solution, word_list)?;
+        if evaluation.iter().all(|eval| *eval == Evaluation::Correct) {
+            self.state = GameState::Won;
+        }
+        self.history.push(Guess {
+            word: guess,
+            evaluation,
+        });
+        if self.state != GameState::Won && self.history.len() == MAX_GUESSES {
+            self.state = GameState::Lost;
+        }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_win() {
+        let word = String::from("tales");
+        let word_list = HashSet::from_iter(std::iter::once(word.clone()));
+        let mut game = Game::new(word.clone(), &word_list).unwrap();
+        game.guess(word.clone(), &word_list).unwrap();
+        assert_eq!(game.state, GameState::Won);
+        assert!(game.guess(word.clone(), &word_list).is_err());
     }
 }
